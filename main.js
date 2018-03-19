37f8e6565690d42ea08cb3a83ebd0d612c83abc2
Uboat46 <uboat46@hotmail.com>
2018-03-19 17:03:05 -0600

    finished compilation module

---
+++ main.js
@@ -0,0 +1,34 @@
+const { spawn } = require('child_process');
+const fs = require('fs');
+const util = require('util');
+
+fs.readFile(process.argv[2] + '.lisp', 'utf8', (err, data) => {
+  if (err) {
+    console.log(`{"error": "${err.message}"}`);
+  }else {
+    transform(data);
+  }
+});
+
+function transform(data) {
+  const patt = /[\t\n]/gm;
+  var trimedData = data;
+  trimedData = trimedData.replace(patt, '');
+  run(trimedData);
+}
+
+
+function run(data) {
+  let ls = spawn('clisp', ['-x', '-q', data])
+  ls.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+  
+  ls.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+  });
+  
+  ls.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+}
