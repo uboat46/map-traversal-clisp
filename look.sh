15d0b2d6376eaee4bd9caf59a374b3fb792b7e82
Uboat46 <uboat46@hotmail.com>
2018-03-20 16:57:07 -0600

    CASASD

--- look.sh
+++ look.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #echo $1 $2
-sudo /usr/bin/clisp -q -x "(PRINT 'HELLO')"
+clisp -q -x "(PRINT 'HELLO')"
 #/usr/bin/clisp -q -i /root/mapTraversalClisp/aStar/ASTAR.fas -x "(look '$1 '$2')"
