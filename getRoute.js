551b8cbe2b40242cc97df4299dd51d51120118d9
Uboat46 <uboat46@hotmail.com>
2018-03-20 16:23:17 -0600

    deleting binary files

--- getRoute.js
+++ getRoute.js
@@ -9,7 +9,7 @@ function runFile(start, end, res) {
   //let ls = spawn('clisp', ['-q','-i', `${__dirname}/aStar/ASTAR.fas`, '-x', `(look '${start} '${end})`]);
   //res.status(200).send(`${__dirname}/aStar/ASTAR.fas`);
   //let ls = spawn('clisp', ['-q','-i', `/root/mapTraversalClisp/aStar/ASTAR.fas`, '-x', `(look '${start} '${end})`], {shell: 'bin/bash'});
-  let ls = spawn('/bin/bash', ['clisp', '-q', '-i', `/root/mapTraversalClisp/aStar/ASTAR.fas`, '-x', `(look '${start} '${end})`]);
+  let ls = spawn('/bin/bash', ['clisp', '-q', '-x', `(look '${start} '${end})`]);
   ls.stdout.on('data', (data) => {
     //if(data.includes('}') || data.includes('(')) {response += data};
     response += data;
