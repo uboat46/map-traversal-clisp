2d44ed29dec4a9aea98c114633a008883be81dfb
Uboat46 <uboat46@hotmail.com>
2018-03-20 17:20:43 -0600

    asdads

--- getRoute.js
+++ getRoute.js
@@ -6,10 +6,10 @@ const preprocess = require('./preprocess.js');
 function runFile(start, end, res) {
   let args = `(look '${start} '${end})`;
   let response = '';
-  //let ls = spawn('clisp', ['-q','-i', `${__dirname}/aStar/ASTAR.fas`, '-x', `(look '${start} '${end})`]);
+  let ls = spawn('clisp', ['-q','-i', `${__dirname}/aStar/ASTAR.fas`, '-x', `(look '${start} '${end})`]);
   //res.status(200).send(`${__dirname}/aStar/ASTAR.fas`);
   //let ls = spawn('clisp', ['-q','-i', `/root/mapTraversalClisp/aStar/ASTAR.fas`, '-x', `(look '${start} '${end})`], {shell: 'bin/bash'});
-  let ls = spawn('/bin/bash', ['/root/mapTraversalClisp/look.sh', '\''+start, '\''+end]);
+  //let ls = spawn('/bin/bash', ['/root/mapTraversalClisp/look.sh', '\''+start, '\''+end]);
   ls.stdout.on('data', (data) => {
     //if(data.includes('}') || data.includes('(')) {response += data};
     response += data;
