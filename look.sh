08af37080630a8f09e9d8ef8edd98b21bac1a668
Uboat46 <uboat46@hotmail.com>
2018-03-20 16:36:28 -0600

    modded shell

--- look.sh
+++ look.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-echo $1 $2
-#/usr/bin/clisp -q -i /root/mapTraversalClisp/aStar/ASTAR.fas -x "(look '$1 '$2')"
+/usr/bin/clisp -q -i /root/mapTraversalClisp/aStar/ASTAR.fas -x "(look '$1 '$2')"
