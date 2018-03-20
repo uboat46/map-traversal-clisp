91f53306f93211828b2f6be346107fa55b3711b1
Uboat46 <uboat46@hotmail.com>
2018-03-19 21:42:28 -0600

    added res to express

--- getRoute.js
+++ getRoute.js
@@ -3,12 +3,12 @@ const fs = require('fs');
 const util = require('util');
 const preprocess = require('./preprocess.js');
 
-function runFile(start, end) {
+function runFile(start, end, res) {
   let response = '';
   let ls = spawn('clisp', ['-q','-i', './aStar/ASTAR.fas', '-x', `(look '${start} '${end})`])
   
   ls.stdout.on('data', (data) => {
-    response = data;
+    response += data;
     console.log(`stdout: ${data}`);
   });
   
@@ -17,16 +17,17 @@ function runFile(start, end) {
   });
   
   ls.on('close', (code) => {
+    res.status(200).send(response);
     console.log(`child process exited with code ${code}`);
   });
 
   return response;
 }
 
-function run(start, end) {
+function run(start, end, res) {
   if(start.trim() != "" && end.trim() != "")
   {
-    return runFile(start, end);
+    return runFile(start, end, res);
   }else {
     return 'Please send arguments to run\n    ex. \n    node run.js rennes avignon';
   }
