71df1ddff865e47c6dc0351f86912e540201fcec
Uboat46 <uboat46@hotmail.com>
2018-03-20 15:36:57 -0600

    prueba

--- getRoute.js
+++ getRoute.js
@@ -4,33 +4,33 @@ const util = require('util');
 const preprocess = require('./preprocess.js');
 
 function runFile(start, end, res) {
+  res.status(200).send(response);
   let response = '';
-  let ls = spawn('clisp', ['-q','-i', `${__dirname}/aStar/ASTAR.fas`, '-x', `(look '${start} '${end})`]);
+  // let ls = spawn('clisp', ['-q','-i', `${__dirname}/aStar/ASTAR.fas`, '-x', `(look '${start} '${end})`]);
   
-  ls.stdout.on('data', (data) => {
-    if(data.includes('}') || data.includes('(')) {response += data};
-    console.log(`stdout: ${data}`);
-  });
+  // ls.stdout.on('data', (data) => {
+  //   if(data.includes('}') || data.includes('(')) {response += data};
+  //   console.log(`stdout: ${data}`);
+  // });
   
-  ls.stderr.on('data', (data) => {
-    console.log(`stderr: ${data}`);
-  });
+  // ls.stderr.on('data', (data) => {
+  //   console.log(`stderr: ${data}`);
+  // });
   
-  ls.on('close', (code) => {
-    response = response.trim().replace('NIL', '').split('}').join(',').substring(0, response.trim().length - 6);
-    response = `{route:${response}}`;
-    res.status(200).send(response);
-    console.log(`child process exited with code ${code}`);
-  });
+  // ls.on('close', (code) => {
+  //   response = response.trim().replace('NIL', '').split('}').join(',').substring(0, response.trim().length - 6);
+  //   response = `{route:${response}}`;
+  //   res.status(200).send(response);
+  //   console.log(`child process exited with code ${code}`);
+  // });
 
-  return response;
+  // return response;
 }
 
 function run(start, end, res) {
   if(start.trim() != "" && end.trim() != "")
   {
-    res.status(200).send("hello");
-    //runFile(start, end, res);
+    runFile(start, end, res);
   }else {
     res.status(200).send('Please send arguments to run\n    ex. \n    node run.js rennes avignon');
   }
