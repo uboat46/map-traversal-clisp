839fb4d1109b7fe00c75e02c5c7d886a4d8e661a
Uboat46 <uboat46@hotmail.com>
2018-04-04 15:37:03 -0500

    finished

--- getRoute.js
+++ getRoute.js
@@ -6,14 +6,14 @@ const preprocess = require('./preprocess.js');
 function runFile(start, end, res) {
   let args = `(look '${start} '${end})`;
   let response = '';
-  //let ls = spawn('clisp', ['-q','-i', `${__dirname}/aStar/ASTAR.fas`, '-x', `(look '${start} '${end})`]);
+  let ls = spawn('clisp', ['-q','-i', `${__dirname}/aStar/ASTAR.fas`, '-x', `(look '${start} '${end})`]);
   //res.status(200).send(`${__dirname}/aStar/ASTAR.fas`);
   //let ls = spawn('clisp', ['-q','-i', `/root/mapTraversalClisp/aStar/ASTAR.fas`, '-x', `(look '${start} '${end})`], {shell: 'bin/bash'});
   //let ls = spawn('/bin/bash', ['/root/mapTraversalClisp/look.sh', '\''+start, '\''+end]);
-  let ls = spawn('clisp', ['-q', '-x', '"(PRINT \'hello"']);
+  //let ls = spawn('clisp', ['-q', '-x', '(PRINT \'hello']);
   ls.stdout.on('data', (data) => {
-    //if(data.includes('}') || data.includes('(')) {response += data};
-    response += data;
+    if(data.includes('}') || data.includes('(')) {response += data};
+    //response += data;
     console.log(`stdout: ${data}`);
   });
   
@@ -23,8 +23,8 @@ function runFile(start, end, res) {
   });
   
   ls.on('close', (code) => {
-    //response = response.trim().replace('NIL', '').split('}').join(',').substring(0, response.trim().length - 6);
-    response = `{route:${response}}`;
+    response = response.trim().replace('NIL', '').split('}').join(',').substring(0, response.trim().length - 6);
+    response = `{"route":"${response}}`.replace("\n","");
     res.status(200).send(response);
     console.log(`child process exited with code ${code}`);
   });
